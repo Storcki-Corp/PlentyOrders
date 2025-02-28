5555e5a8bb5c34cd19558be474ae0ba55cc1046f
Raphael chaboisseau <raphael.chaboisseau@gmail.com>
2025-02-28 21:53:40 +0100

    ajout color blind; lagnues; contenu pages

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-// Assurez-vous que le DOM est entièrement chargé avant d'exécuter le script
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM entièrement chargé et analysé");
-});
 
-// Exemple de fonction pour un bouton pour revenir à la page d'accueil
-function BackToHome() {
-  window.location.href = "/plenty-orders/home/";
-}
 
 function ToDownload() {
   window.location.href = "/plenty-orders/download/";
   console.log("Téléchargement");
 }
+
+// Fonction pour changer de page, principalemnt pour changer de langue et le mode daltonien
+function NewPath(file){
+ 
+  window.location.href = file;
+}
+
